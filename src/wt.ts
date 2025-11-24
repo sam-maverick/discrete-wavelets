6263e584117c37c1ea5e7ddaf0e18d1480cd3c87
Sam <jsamper@fc.ul.pt>
2025-11-24 15:07:35 +0000

    Added support for 2-D wavelet transforms

--- src/wt.ts
+++ src/wt.ts
@@ -42,6 +42,36 @@ import {
  */
 const DEFAULT_PADDING_MODE: PaddingMode = PADDING_MODES.symmetric;
 
+
+export interface WaveletCoefficients2D {
+    approximation: number[][];  // This is the LL band
+    details: { 
+        LH: number[][],
+        HL: number[][],
+        HH: number[][]
+    }[];  // Mind that this is an array of objects!
+    // Information of the original input size must be kept to reliably undo the padding in the last step of waverec2()
+    size: [number, number];
+    mask?: { // This will store an optional mask matrix of coefficients, where 0 means that that position on the transform
+             // result is a synthetic zero produced by the padding, and anything else means 'position with actual data'
+        approximation: number[][];
+        details: { 
+            LH: number[][],
+            HL: number[][],
+            HH: number[][] 
+        }[];  // Mind that this is an array of objects!
+    }
+}
+
+export interface WaveletBands2D {
+    LL: number[][],
+    LH: number[][],
+    HL: number[][],
+    HH: number[][],
+}
+
+
+
 /**
  * Collection of methods for Discrete Wavelet Transform (DWT).
  */
@@ -50,6 +80,251 @@ export default class DiscreteWavelets {
    * Contains static information about the signal extension modes.
    */
   static readonly Modes: Readonly<PaddingModes> = PADDING_MODES;
+
+
+
+/**
+ *     2-D FUNCTIONS
+ */
+
+  static maxLevel2(
+      size: [number, number], 
+      wavelet: Wavelet,
+  ): number {
+      // !!! Assuming that the decimation factor is 2, it should be equal to:
+      // Math.floor(Math.log2(Math.min(rows, cols)))
+      return Math.min(
+          this.maxLevel(size[0], wavelet),
+          this.maxLevel(size[1], wavelet)
+      );
+  }
+
+  static dwtRows(
+      matrix: number[][], 
+      wavelet: Wavelet, 
+      paddingmode: PaddingMode,
+  ): {cA: number[][], cD: number[][]} {
+      const rows = matrix.length;
+      //const cols = matrix[0].length;
+
+      const cA: number[][] = [];
+      const cD: number[][] = [];
+
+      for (let r = 0; r < rows; r++) {
+          const [approx, detail] = this.dwt(matrix[r], wavelet, paddingmode);  // approx.length = detail.length = padding + cols / 2
+          cA.push(approx);
+          cD.push(detail);
+      }
+      
+      return { cA, cD };  // cA.length = cD.length = rows
+  }
+
+  static dwtCols(
+      cA: number[][], 
+      cD: number[][], 
+      wavelet: Wavelet, 
+      paddingmode: PaddingMode,
+  ): WaveletBands2D {
+      //const rows = cA.length;
+      const cols = cA[0].length;
+
+      // This initialization is necessary for later to be able to access bands.something; otherwise bands is undefined so that we cannot access sub-fields
+      let bands: WaveletBands2D = { LL: [], LH: [], HL: [], HH: [] };
+
+      for (let col = 0; col < cols; col++) {
+          const recA = cA.map(r => r[col]);
+          const [A1, D1] = this.dwt(recA, wavelet, paddingmode);  // A1.length = D1.length = padding + cA.length / 2
+          const recD = cD.map(r => r[col]);
+          const [A2, D2] = this.dwt(recD, wavelet, paddingmode);  // A2.length = D2.length = padding + cD.length / 2
+
+          // Initialize the bands as [][] on the first iteration, now that we know the result of WT.dwt() *with the padding*
+          if (col == 0) {
+              bands.LL = Array.from({ length: cols }, () => Array(A1.length).fill(0));  // A1.length = D1.length
+              bands.LH = Array.from({ length: cols }, () => Array(D1.length).fill(0));
+              bands.HL = Array.from({ length: cols }, () => Array(A2.length).fill(0));  // A2.length = D2.length
+              bands.HH = Array.from({ length: cols }, () => Array(D2.length).fill(0));
+          }
+
+          // assign column results for cA
+          for (let i = 0; i < A1.length; i++) {
+              bands.LL[col][i] = A1[i];
+              bands.LH[col][i] = D1[i];
+          }
+          // assign column results for cD
+          for (let i = 0; i < A2.length; i++) {
+              bands.HL[col][i] = A2[i];
+              bands.HH[col][i] = D2[i];
+          }
+      }
+
+      return bands;
+  }
+
+  static idwtRows(
+      cA: number[][], 
+      cD: number[][], 
+      wavelet: Wavelet,
+  ): number[][] {
+      const rows = cA.length;
+      const result: number[][] = [];
+
+      for (let r = 0; r < rows; r++) {
+          const row = this.idwt(cA[r], cD[r], wavelet);
+          result.push(row);
+      }
+
+      return result;
+  }
+
+  static idwtCols(
+      bands: WaveletBands2D,
+      wavelet: Wavelet,
+  ): { cA: number[][], cD: number[][] } {
+
+      let trimmedLL = bands.LL.map(row => [...row]);
+
+      // Undo the padding(s)
+      // !!! Assuming that the decimation factor is 2
+      if (bands.LL.length > bands.LH.length) {
+          trimmedLL.pop();
+      }
+      if (bands.LL[0].length > bands.LH[0].length) {
+          for (const row of trimmedLL) {
+              row.pop();
+          }
+      }
+
+      const cols = trimmedLL.length;
+      const rows = trimmedLL[0].length * 2;
+
+      const cA: number[][] = Array.from({ length: rows }, () => Array(cols).fill(0));
+      const cD: number[][] = Array.from({ length: rows }, () => Array(cols).fill(0));
+
+      for (let col = 0; col < cols; col++) {
+          const recA = this.idwt(trimmedLL[col]/*approx*/, bands.LH[col]/*details*/, wavelet);
+          const recD = this.idwt(bands.HL[col]/*approx*/, bands.HH[col]/*details*/, wavelet);
+
+          for (let r = 0; r < recA.length; r++) {
+              cA[r][col] = recA[r];
+              cD[r][col] = recD[r]
+          } 
+      }
+
+      return { cA, cD };
+  }
+
+  static dwt2(
+      data: number[][],
+      wavelet: Wavelet,
+      mode: PaddingMode = 'symmetric',
+  ): WaveletBands2D {
+      const { cA, cD } = this.dwtRows(data, wavelet, mode);
+      const bands = this.dwtCols(cA, cD, wavelet, mode);
+      return bands;
+  }
+
+  static wavedec2(
+      data: number[][],
+      wavelet: Wavelet,
+      mode: PaddingMode = 'symmetric',
+      level?: number,
+  ): WaveletCoefficients2D {
+
+      const rows = data.length;
+      const cols = data[0].length;
+
+      const maxLevels = this.maxLevel2([rows, cols], wavelet);
+      let numLevels: number;
+      if (level === undefined) {
+          numLevels = maxLevels;
+      } else {
+          if ( ! (Number.isInteger(level) && level > 0) ) {
+              throw new Error('Level parameter must be an integer greater than zero');
+          }
+          if (level > maxLevels) {
+              throw new Error('Can\'t decompose more times than the maximum number of levels '+maxLevels);
+          }
+          numLevels = level;
+      }
+
+      let current: number[][] = data;
+      // We will use the taint analysis technique to track which coefficients are affected by original data (1) and which not(0)
+      // Coefficients that are not affected by original data must be a result of padding; they are synthetic
+      let currentMask: number[][] = Array.from({ length: data.length }, () => Array(data[0].length).fill(1));  // Creates an array with the same shape as data, but with all values as 1
+
+      const result: WaveletCoefficients2D = {
+          // We need to initialize approximation:data, because there is the possibility that numLevels==0 
+          approximation: data,  // Reminder: WaveletCoefficients2D['approximation'] is number[][]
+          details: [],  // Reminder: WaveletCoefficients2D['details'] variable types are an array of objects, where each object has 3 elements, and each element is a number[][]
+          size: [rows,cols],
+          mask: {
+              approximation: Array.from({ length: data.length }, () => Array(data[0].length).fill(1)),
+              details: [],
+          }
+      }
+
+      for (let level = 0; level < numLevels; level++) {
+
+          const bands: WaveletBands2D = this.dwt2(current, wavelet, mode);  // Perform one level of decomposition
+          const bandsMask: WaveletBands2D = this.dwt2(currentMask, wavelet, mode);  // We do taint analysis to detect synthetic coefficients
+
+          // We keep LL for the next iteration or as the last-level approximation
+          result.approximation = bands.LL;
+          if (result.mask)  result.mask.approximation = bandsMask.LL;
+          // We push this result to the matrix, so that details[0] will be the first-level decomposition and details[details.length-1] will be the last-level decomposition
+          result.details.push({ LH: bands.LH, HL: bands.HL, HH: bands.HH });
+          if (result.mask)  result.mask.details.push({ LH: bandsMask.LH, HL: bandsMask.HL, HH: bandsMask.HH });
+
+          current = result.approximation; // Recurse only on the LL band
+          if (result.mask)  currentMask = result.mask.approximation;
+      }
+
+      return result;
+  }
+
+  static idwt2(
+      approx: number[][],
+      detail: { LH: number[][], HL: number[][], HH: number[][] },
+      wavelet: Wavelet,
+  ): number[][] {
+      const { cA, cD } = this.idwtCols({LL: approx, LH: detail.LH, HL: detail.HL, HH:detail.HH},  wavelet);
+      let data = this.idwtRows(cA, cD, wavelet); 
+      return data;
+  }
+
+  static waverec2(
+      coeffs: WaveletCoefficients2D,
+      wavelet: Wavelet,
+  ): number[][] {
+
+      let current: number[][] = coeffs.approximation;
+
+      for (let level = coeffs.details.length - 1; level >= 0; level--) {
+
+          current = this.idwt2(current, coeffs.details[level], wavelet);  // Perform one level of recomposition
+
+      }
+
+      // Undo the padding(s) of the last step
+      // !!! Assuming that the decimation factor is 2
+      if (current.length > coeffs.size[0]) {
+          current.pop();
+      }
+      if (current[0].length > coeffs.size[1]) {
+          for (const row of current) {
+              row.pop();
+          }
+      }
+
+      return current;
+  } 
+
+
+
+
+/**
+ *     1-D FUNCTIONS
+ */
 
   /**
    * Single level Discrete Wavelet Transform.
